591499000499cd1daf4be10f4f951a41ae291941
sihabbayraktar@gmail.com <sihabbayraktar@gmail.com>
2021-12-27 14:53:17 +0100

    Data generated

--- ExportPoints.py
+++ ExportPoints.py
@@ -1,33 +1,69 @@
 import vtk
 import os
 import sys
+import argparse
+
+
 
 # Change working directory to allow script to be run from the ParaView shell
 datapath = os.path.dirname(os.path.abspath(__file__))
 os.chdir(datapath)
 
+
 # This should allow modules to be imported correctly in the ParaView shell
 sys.path.append(datapath)
-from ReadPointsCSV import readPoints
+
+#from ReadPointsCSV import readPoints
 #if using python from pandas, uncomment line below and comment line above
-#from ReadPoints3 import readPoints
+from ReadPoints3 import readPoints
+from ReadPoints3 import readPoints_magnitude_scale
+from ReadPoints3 import readPoints_earthquake_occurences
+
+
+parser = argparse.ArgumentParser(description='ExportPoints')
+parser.add_argument('--input_file', type=str, default='data_90days.txt')
+parser.add_argument('--magnitude', type=int, default=0)
+parser.add_argument('--output_file', type=str, default='earthquake.vtk')
+parser.add_argument('--occurences', type=int, default=1)
+
+
+# Change working directory to allow script to be run from the ParaView shell
+datapath = os.path.dirname(os.path.abspath(__file__))
+os.chdir(datapath)
+
+args = parser.parse_args()
 
 # Read the data in CSV format
-filename = "data_90days.txt"
-points, scalars, tid, depth = readPoints(filename)
-scalars.SetName("magnitude")
-tid.SetName("time")
-depth.SetName("depth")
-data = vtk.vtkPolyData()
-data.SetPoints(points)
-data.GetPointData().AddArray(scalars)
-data.GetPointData().AddArray(tid)
-data.GetPointData().AddArray(depth)
-data.GetPointData().SetActiveScalars("magnitude")
+filename = args.input_file
+magnitude_scaling = args.magnitude
+out_vtk = args.output_file
+occurences = args.occurences
+#points, scalars, tid, depth = readPoints(filename)
+
+if occurences == 1:
+	points, scalars, tid, depth = readPoints_magnitude_scale(file=filename, sep="|", depth_scaling=0.01, time_shift=-1467247524, magnitude_scaling=magnitude_scaling)
+	scalars.SetName("magnitude")
+	tid.SetName("time")
+	depth.SetName("depth")
+	data = vtk.vtkPolyData()
+	data.SetPoints(points)
+	data.GetPointData().AddArray(scalars)
+	data.GetPointData().AddArray(tid)
+	data.GetPointData().AddArray(depth)
+	data.GetPointData().SetActiveScalars("magnitude")
+
+else:
+	points, scalars = readPoints_earthquake_occurences(file=filename, sep="|", quake_number = occurences)
+	scalars.SetName("occurences")
+	data = vtk.vtkPolyData()
+	data.SetPoints(points)
+	data.GetPointData().AddArray(scalars)
+	data.GetPointData().SetActiveScalars("occurences")
+
 
 # Write data to VTK legacy format that Paraview can import
 writer = vtk.vtkPolyDataWriter()
-writer.SetFileName("earthquake.vtk")
+writer.SetFileName(out_vtk)
 writer.SetFileTypeToBinary()
 writer.SetInputData(data)
 writer.Write()
--- ReadPoints3.py
+++ ReadPoints3.py
@@ -5,9 +5,11 @@ import pandas as pd
 import numpy as np
 import vtk
 from vtk.util import numpy_support
+import pandasql as ps
+
 
 def readPoints(file, sep="|", depth_scaling=0.01, time_shift=-1467247524):
-
+    '''
     df=pd.read_csv(file,sep=sep)
     #df["Magnitude"].isna().values.any()
 
@@ -28,3 +30,55 @@ def readPoints(file, sep="|", depth_scaling=0.01, time_shift=-1467247524):
     d = numpy_support.numpy_to_vtk(depth)
 
     return p,s,t,d
+    '''
+    return readPoints_magnitude_scale(file, sep, depth_scaling, time_shift, 0)
+
+def readPoints_magnitude_scale(file, sep="|", depth_scaling=0.01, time_shift=-1467247524, magnitude_scaling=0):
+    df = pd.read_csv(file, sep=sep)
+    
+    if magnitude_scaling != 0:
+        df = df[df.apply(lambda x: x["Magnitude"] >= magnitude_scaling, axis=1)]
+    
+    df[['Latitude', 'Longitude']]=df[['Latitude', 'Longitude']].clip(lower=0,upper=360)
+    df['NewDepth']=df['Depth/Km']*depth_scaling
+    df["NewTime"]= pd.to_datetime(df['Time']).values.astype(np.int64) // 10 ** 6
+
+    points=df[['Latitude', 'Longitude', 'NewDepth']].to_numpy()
+    times=df["NewTime"].to_numpy()+time_shift
+    scalars=df["Magnitude"].to_numpy()
+    depth=df["NewDepth"].to_numpy()
+
+    p = vtk.vtkPoints()
+    for i in range(points.shape[0]):
+        p.InsertNextPoint(points[i,:])
+    s = numpy_support.numpy_to_vtk(scalars)
+    t = numpy_support.numpy_to_vtk(times)
+    d = numpy_support.numpy_to_vtk(depth)
+
+    return p,s,t,d
+
+
+def readPoints_earthquake_occurences(file, sep="|", quake_number=1):
+    df = pd.read_csv(file, sep=sep)
+
+    #NECESSARY OPERATIONS FOR FILTERING
+    df_copy = df.copy()
+    df_copy=df_copy.rename(columns={"#EventID": "ID"})
+    df_copy=ps.sqldf("select COUNT(ID), Latitude, Longitude, EventLocationName from df_copy group by EventLocationName")
+    df_copy=df_copy.rename(columns={"COUNT(ID)": "Count"})
+
+    df_filter = df_copy[df_copy.Count > quake_number]
+
+    df_filter[['Latitude', 'Longitude']]=df_filter[['Latitude', 'Longitude']].clip(lower=0,upper=360)
+    
+
+    points=df_filter[['Latitude', 'Longitude']].to_numpy()
+    scalars = df_filter["Count"].to_numpy()
+
+    p = vtk.vtkPoints()
+    for i in range(len(points)):
+        p.InsertNextPoint(points[i][0], points[i][1], 0) ## FIX MIGHT BE REQUIRED
+ 
+    s = numpy_support.numpy_to_vtk(scalars)
+
+    return p,s
